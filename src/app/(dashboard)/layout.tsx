a2cf44a3c2a5e406afe0974cef5918b747f3cb15
Marlon Rosa <marlonrosa@MacBook-Air-de-Marlon.local>
2024-11-28 03:22:24 -0300

    Menu and NavBar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,8 @@
+import Menu from "@/components/Menu";
+import NavBar from "@/components/NavBar";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -6,14 +11,23 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex"> 
       {/* LEFT */}
-      <div className="w-[14%] md:w[8%] lg:w-[16%] xl:w-[14%] bg-red-200">L</div>
+      <div className="w-[14%] md:w[8%] lg:w-[16%] xl:w-[14%]">
+          <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32}/>
+          <span className="hidden lg:block">USCHOOL</span>
+          </Link>
+          <Menu/>
+      </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">L</div>
+      <div className="w-[86%] md:w[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <NavBar/> 
+      </div>
     </div>
   );
 }
-
-
 
 
 
